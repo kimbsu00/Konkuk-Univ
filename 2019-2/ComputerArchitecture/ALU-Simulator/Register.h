789afef412036dc9279a041dcad88d1e7d98caf4
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2019-11-25 21:51:02 +0900

    Update Register.h

--- 2019-2/ComputerArchitecture/ALU-Simulator/Register.h
+++ 2019-2/ComputerArchitecture/ALU-Simulator/Register.h
@@ -20,16 +20,16 @@ public:
 	Register();
 	~Register();
 
-	void showBit(bool flag=false) const;	  //현재 레지스터에 저장된 값을 출력한다. flag가 true인 경우 flag도 출력한다.
+	void showBit(bool flag=false) const;	 		 //현재 레지스터에 저장된 값을 출력한다. flag가 true인 경우 flag도 출력한다.
 	void setNum(int num);					//십진수의 값을 이진수변환하여 레지스터에 저장한다.
 	int getNum(void) const;					//레지스터에 저장된 값을 반환한다.
 
-	bool shiftLeft(void);	//Register에서   왼쪽으로 쉬프트연산을 수행한다.
-	bool shiftRight(void);	//Register에서 오른쪽으로 쉬프트연산을 수행한다.
+	bool shiftLeft(void);					//Register에서   왼쪽으로 쉬프트연산을 수행한다.
+	bool shiftRight(void);					//Register에서 오른쪽으로 쉬프트연산을 수행한다.
 
-	static Register complement(Register a);								//a의 2의보수를 저장한 레지스터를 반환한다.
+	static Register complement(Register a);			//a의 2의보수를 저장한 레지스터를 반환한다.
 	static Register Adder_Subtracter(const Register & a, const Register & b, const bool & isSub);
 	// ^ isSub가 true일 때, a-b, false일 때 a+b를 한 결과를 반환한다.
-	static long long multiplication(const Register & m, Register q);	//곱셈을 수행한다.
+	static long long multiplication(const Register & m, Register q);		//곱셈을 수행한다.
 	static void division(Register q, const Register & m);				//나눗셉을 수행한다.
 };
